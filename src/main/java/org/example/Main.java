7d0b552c8b308c496cea0de761aa9cfc5bd7131c
Anastasiya Novikova <91197697+Ananastyyy@users.noreply.github.com>
2022-10-08 23:01:20 +0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,10 @@ package org.example;
  * @author Yaroslav Strahov and Novikova Anastasia
  */
 
+/**
+ * Метод, который осуществляет создание экземпляра консольного бота, после чего начинает работу
+ */
+
 public class Main {
     public static void main(String[] args) {
         ConsoleBot bot = new ConsoleBot();
