cabb646727ae89d77700eac79ab60073320b94fd
Anastasiya Novikova <91197697+Ananastyyy@users.noreply.github.com>
2022-10-08 22:54:02 +0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,11 @@
 package org.example;
 
+/**
+ * Программа, реализующая парсинг сайта dnd.su
+ *
+ * @author Yaroslav Strahov and Novikova Anastasia
+ */
+
 public class Main {
     public static void main(String[] args) {
         ConsoleBot bot = new ConsoleBot();
